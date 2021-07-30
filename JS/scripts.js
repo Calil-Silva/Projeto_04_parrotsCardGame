da89e14a2b7b82224b38a809b61f7e114dfeaccc
Calil_Silva <ka.renner@hotmail.com>
2021-07-30 07:32:46 -0300

    Ao clicar na carta a mesma deve ser virada

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,8 +3,8 @@ let i = 0;
 let index = 0;
 let addCartas = "";
 
-function comparador() { 
-    return Math.random() - 0.5; 
+function comparador() {
+    return Math.random() - 0.5;
 }
 
 let teste = [];
@@ -14,14 +14,12 @@ while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0) {
 }
 
 function darCartas() {
-    
-    
+
+
     let cartas = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
     cartas.sort(comparador);
 
-    let baralho = document.querySelector(".lista");
-
-
+    let baralho = document.querySelector("div");
 
     while (i < (qtdCartas / 2)) {
 
@@ -36,28 +34,56 @@ function darCartas() {
 
     while (index < teste.length) {
 
-        addCartas += `<li>${teste[index]}</li>`;
+        addCartas += `<ul class="lista" onclick="virarCarta(this)"><li class="back-face face">${teste[index]}</li><li class="front-face face"><img src="../files/front.png"></li></ul>`;
 
         baralho.innerHTML = addCartas;
 
         index++;
-        }
+    }
 }
 
 darCartas();
 
-function numCartas() {
-    
-    let display = document.querySelector(".lista");
-    if(qtdCartas === 8) {
-        display.classList.add("oitoCartas");
-    } else if(qtdCartas === 10) {
-        display.classList.add("dezCartas");
-    } else if(qtdCartas === 12) {
-        display.classList.add("dozeCartas");
-    } else if(qtdCartas === 14) {
-        display.classList.add("quatorzeCartas");
-    }
+// function numCartas() {
+
+//     let display = document.querySelector(".lista");
+//     if (qtdCartas === 8) {
+//         display.classList.add("oitoCartas");
+//     } else if (qtdCartas === 10) {
+//         display.classList.add("dezCartas");
+//     } else if (qtdCartas === 12) {
+//         display.classList.add("dozeCartas");
+//     } else if (qtdCartas === 14) {
+//         display.classList.add("quatorzeCartas");
+//     }
+// }
+
+// numCartas();
+
+let myArray = [];
+let backFace;
+let frontFace;
+let parrot;
+let alter = [];
+let backFaceFirst;
+let frontFaceFirst;
+
+function virarCarta(elemento) {
+    let parrot = elemento.querySelector("img");
+    let parrotPai = parrot.parentNode.parentNode;
+    let tituloCards = parrot.alt;
+
+    myArray.push(parrotPai);
+    alter.push(tituloCards);
+
+    backFace = elemento.querySelector(".back-face");
+    frontFace = elemento.querySelector(".front-face");
+    backFace.classList.add("showFront");
+    frontFace.classList.add("hideBack");
+
+    setTimeout(desvirarCartas, 1000);
+    cartasIguais();
 }
 
-numCartas();
+
+
